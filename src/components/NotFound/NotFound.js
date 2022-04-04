ba71d36da36b48110c8f7cd5d777f75d642ef8c3
Rejaur9131 <raju_seu@yahoo.com>
2022-04-04 19:21:50 +0600

    blog section done

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFound = () => {
   return (
     <div>
-      <h1>404, Not Found</h1>
+      <h1 className="text-4xl text-red-700 py-72">404, Not Found</h1>
     </div>
   );
 };
