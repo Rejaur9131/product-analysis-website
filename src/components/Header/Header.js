e0a4b74732c423a42ef6d2e9c13d57f1796def19
Rejaur9131 <raju_seu@yahoo.com>
2022-04-04 06:19:35 +0600

    home page, product info section, and navigaton to review done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,20 +4,20 @@ import { NavLink } from 'react-router-dom';
 const Header = () => {
   return (
     <nav>
-      <div className="link-container bg-indigo-600 p-3 font-semibold text-xl">
-        <NavLink className={({ isActive }) => (isActive ? 'text-red-600 mr-4' : 'text-white mr-4')} to="/">
+      <div className="link-container bg-indigo-600 p-4 font-semibold text-xl">
+        <NavLink className={({ isActive }) => (isActive ? 'text-red-600 mr-4 border-b-2 border-red-600' : 'text-white mr-4')} to="/">
           Home
         </NavLink>
-        <NavLink className={({ isActive }) => (isActive ? 'text-red-600 mr-4' : 'text-white mr-4')} to="/reviews">
+        <NavLink className={({ isActive }) => (isActive ? 'text-red-600 mr-4 border-b-2 border-red-600' : 'text-white mr-4')} to="/reviews">
           Reviews
         </NavLink>
-        <NavLink className={({ isActive }) => (isActive ? 'text-red-600 mr-4' : 'text-white mr-4')} to="/dashboard">
+        <NavLink className={({ isActive }) => (isActive ? 'text-red-600 mr-4 border-b-2 border-red-600' : 'text-white mr-4')} to="/dashboard">
           Dashaboard
         </NavLink>
-        <NavLink className={({ isActive }) => (isActive ? 'text-red-600 mr-4' : 'text-white mr-4')} to="/blogs">
+        <NavLink className={({ isActive }) => (isActive ? 'text-red-600 mr-4 border-b-2 border-red-600' : 'text-white mr-4')} to="/blogs">
           Blogs
         </NavLink>
-        <NavLink className={({ isActive }) => (isActive ? 'text-red-600 mr-4' : 'text-white mr-4')} to="/about">
+        <NavLink className={({ isActive }) => (isActive ? 'text-red-600 mr-4 border-b-2 border-red-600' : 'text-white mr-4')} to="/about">
           About
         </NavLink>
       </div>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import pic from '../../images/instant-pot.jpg';
 
 const Home = () => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <h2>this is homes</h2>
+    <div className="home-container">
+      <div className="product-container flex justify-between align-middle p-8">
+        <div className="mx-8">
+          <h1 className="text-6xl font-extrabold font-mono text-indigo-500 text-left">Cooker of Your Choice</h1>
+          <h2 className="text-4xl font-extrabold font-mono text-indigo-400 text-left mb-4">Ensures Reliablity and Durablility</h2>
+          <p className="text-xl text-left">Pressure cook, slow cook, sous vide, sauté pan, sterilizer, yogurt maker, food warmer, cake baker and steamer. Cook for up to 6 people - perfect for growing families, or meal prepping and batch cooking for singles. Overheat Protection, safe locking lid, upgraded gentle steam release switch with diffusing cover to reduce noise and prevent splashing on the counter, and more. Products with electrical plugs are designed for use in the US. Outlets and voltage differ internationally and this product may require an adapter or converter for use in your destination. Please check compatibility before purchasing.</p>
+        </div>
+        <img className="w-96" src={pic} alt="" />
+      </div>
+      <div className="review-container">
+        <h1 className="text-3xl font-bold mb-4">Customers Reviews: 3</h1>
+        <button
+          className="bg-indigo-600 p-2 text-white font-bold rounded"
+          onClick={() => {
+            navigate('/reviews');
+          }}
+        >
+          See All Reviews
+        </button>
+      </div>
     </div>
   );
 };
