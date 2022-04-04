444bdd9dbe80cd86a717ef2e152a589235fd2a8b
Rejaur9131 <raju_seu@yahoo.com>
2022-04-04 23:48:47 +0600

    dashboard part done & ready for submission

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,49 +1,12 @@
-import React, { PureComponent } from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
-const data = [
-  {
-    month: 'Mar',
-    investment: 100000,
-    sell: 241,
-    revenue: 10401
-  },
-  {
-    month: 'Apr',
-    investment: 200000,
-    sell: 423,
-    revenue: 24500
-  },
-  {
-    month: 'May',
-    investment: 500000,
-    sell: 726,
-    revenue: 67010
-  },
-  {
-    month: 'Jun',
-    investment: 500000,
-    sell: 529,
-    revenue: 40405
-  },
-  {
-    month: 'Jul',
-    investment: 600000,
-    sell: 601,
-    revenue: 50900
-  },
-  {
-    month: 'Aug',
-    investment: 700000,
-    sell: 670,
-    revenue: 61000
-  }
-];
+import React from 'react';
+import MyBarChart from '../MyBarChart/MyBarChart';
+import MyLineChart from './../MyLineChart/MyLineChart';
 
 const Dashboard = () => {
   return (
-    <div>
-      <h2>this is dashboard</h2>
+    <div className="flex justify-around">
+      <MyLineChart></MyLineChart>
+      <MyBarChart></MyBarChart>
     </div>
   );
 };
